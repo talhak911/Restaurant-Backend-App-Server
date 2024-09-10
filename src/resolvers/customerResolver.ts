aece05f348db5f48e9eaa4ffb113999ae783841d
Talha Sadiq <talhak911>
2024-09-10 13:52:16 +0500

    feat: Add address resolver . (working)

---
+++ src/resolvers/customerResolver.ts
@@ -0,0 +1,59 @@
+import {
+  Arg,
+  Ctx,
+  Info,
+  Mutation,
+  Resolver,
+  UseMiddleware,
+} from "type-graphql";
+
+import prisma from "../../prisma/client";
+import { isAuth } from "../middleware/middleware";
+import {
+  CreateOneFoodArgs,
+  CreateOneFoodResolver,
+  FoodCreateWithoutRestaurantInput,
+  RestaurantUpdateInput,
+  UpdateOneRestaurantArgs,
+  UpdateOneRestaurantResolver,
+} from "../../prisma/generated/type-graphql";
+import { GraphQLResolveInfo } from "graphql";
+
+@Resolver()
+@UseMiddleware(isAuth)
+export class CustomerResolver {
+  @Mutation(() => Boolean || String)
+  async addAddress(
+    @Arg("address") address: string,
+    //   @Info() info: GraphQLResolveInfo,
+    //   @Arg("data") data: FoodCreateWithoutRestaurantInput,
+    @Ctx() ctx: any
+  ): Promise<boolean | string> {
+    try {
+      const userId = ctx.payload.id;
+      const customer = await prisma.customer.findUnique({
+        where: { userId },
+      });
+
+      if (!customer) {
+        throw new Error("Customer not found");
+      }
+
+      await prisma.customer.update({
+        where: { userId },
+        data: {
+          address: {
+            push: address, // Append the new address to the array
+          },
+        },
+      });
+
+      return true;
+    } catch (error: any) {
+      console.log(error);
+      return error.message;
+    }
+  }
+
+  
+}
