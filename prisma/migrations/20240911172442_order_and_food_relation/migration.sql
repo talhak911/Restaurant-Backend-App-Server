8a53daea6f7a7b842af707660ea53005f9b804ac
Talha Sadiq <talhak911>
2024-09-12 09:55:07 +0500

    chore: testing, many to many relation between order and foods

---
+++ prisma/migrations/20240911172442_order_and_food_relation/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_FoodToOrder" (
+    "A" TEXT NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_FoodToOrder_AB_unique" ON "_FoodToOrder"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_FoodToOrder_B_index" ON "_FoodToOrder"("B");
+
+-- AddForeignKey
+ALTER TABLE "_FoodToOrder" ADD CONSTRAINT "_FoodToOrder_A_fkey" FOREIGN KEY ("A") REFERENCES "Food"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_FoodToOrder" ADD CONSTRAINT "_FoodToOrder_B_fkey" FOREIGN KEY ("B") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
