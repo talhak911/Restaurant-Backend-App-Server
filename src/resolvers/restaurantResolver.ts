d327c14249d81c6e10f3ee7a1b6e625aa0868756
Talha Sadiq <talhak911>
2024-09-12 21:34:21 +0500

    feat: add fetchfoods and update foods

--- src/resolvers/restaurantResolver.ts
+++ src/resolvers/restaurantResolver.ts
@@ -3,6 +3,7 @@ import {
   Ctx,
   Info,
   Mutation,
+  Query,
   Resolver,
   UseMiddleware,
 } from "type-graphql";
@@ -11,8 +12,13 @@ import { isAuth } from "../middleware/middleware";
 import {
   CreateOneFoodArgs,
   CreateOneFoodResolver,
+  Food,
+  FoodCategory,
   FoodCreateWithoutRestaurantInput,
+  FoodUpdateWithoutCartsInput,
   RestaurantUpdateInput,
+  UpdateOneFoodArgs,
+  UpdateOneFoodResolver,
   UpdateOneRestaurantArgs,
   UpdateOneRestaurantResolver,
 } from "../../prisma/generated/type-graphql";
@@ -40,9 +46,55 @@ export class RestaurantResolver {
       const createoneFoodResolver = new CreateOneFoodResolver();
       const args = new CreateOneFoodArgs();
       args.data = { ...data, restaurant: { connect: { userId } } };
-      const res = await createoneFoodResolver.createOneFood(ctx, info, args);
-      console.log("FInal res is ", res);
+      await createoneFoodResolver.createOneFood(ctx, info, args);
       return true;
+    } catch (error: any) {
+      console.log(error);
+      return error.message;
+    }
+  }
+  @Mutation(() => String || Boolean)
+  async updateFoodItem(
+    @Info() info: GraphQLResolveInfo,
+
+    @Arg("foodId") foodId: string,
+    @Arg("data") data: FoodUpdateWithoutCartsInput,
+    @Ctx() ctx: MyContext
+  ): Promise<boolean | string> {
+    try {
+      const userId = ctx?.user?.id;
+      const food = await ctx.prisma.food.findUnique({
+        where: { id: foodId, restaurant: { userId } },
+      });
+      if (!food) {
+        throw new Error("No food item found");
+      }
+
+      const updateOneFoodResolver = new UpdateOneFoodResolver();
+      const args = new UpdateOneFoodArgs();
+      args.where = { id: foodId };
+      args.data = { ...data };
+      updateOneFoodResolver.updateOneFood(ctx, info, args);
+
+      return true;
+    } catch (error: any) {
+      console.log(error);
+      return error.message;
+    }
+  }
+
+  @Query(() => [Food])
+  async fetchFoods(
+    @Arg("category", { nullable: true }) category: FoodCategory,
+    @Ctx() ctx: MyContext
+  ): Promise<Food[]> {
+    try {
+      if (category) {
+        const foods = await ctx.prisma.food.findMany({ where: { category } });
+        return foods;
+      }
+      const foods = await ctx.prisma.food.findMany();
+      return foods;
     } catch (error: any) {
       console.log(error);
       return error.message;
