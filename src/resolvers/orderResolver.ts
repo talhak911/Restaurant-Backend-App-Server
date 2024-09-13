d0374ed279d7d41a08362253df61597129c34688
Talha Sadiq <talhak911>
2024-09-13 20:27:33 +0500

    feat: cancel order mutataion added

--- src/resolvers/orderResolver.ts
+++ src/resolvers/orderResolver.ts
@@ -70,19 +70,54 @@ export class OrderResolver {
   @Query(() => [Order])
   async fetchOrders(
     @Ctx() ctx: MyContext,
-    @Arg("status",{nullable:true}) status: OrderStatus,
+    @Arg("status", { nullable: true }) status: OrderStatus,
     @Info() info: GraphQLResolveInfo
   ): Promise<Order[]> {
     const userId = ctx.user?.id as string;
     const findManyOrderResolver = new FindManyOrderResolver();
     const args = new FindManyOrderArgs();
-if(status){
-  args.where = { status: { equals: status }, customerId: { equals: userId } };
-}
+    if (status) {
+      args.where = {
+        status: { equals: status },
+        customerId: { equals: userId },
+      };
+    }
     const orders = await findManyOrderResolver.orders(ctx, info, args);
     return orders;
   }
 
+  @Mutation(() => String ||Boolean)
+  async cancelOrder(
+    @Ctx() ctx: MyContext,
+    @Arg("orderId") orderId: number
+  ): Promise<string | boolean> {
+    try {
+      const userId = ctx.user?.id as string;
+      const order = await ctx.prisma.order.findUnique({
+        where: { id: orderId },
+      });
+      if (order?.customerId !== userId) {
+        throw new Error("You are not authorized to cancel this order");
+      }
+      if (!order) {
+        throw new Error("order not found");
+      }
+      if (order?.status !== "PENDING") {
+        throw new Error("Order is being prepared you cannot cancel it now");
+      }
+      await ctx.prisma.order.update({
+        where: { customerId: userId, id: orderId },
+        data: {
+          status: "CANCELED",
+        },
+      });
+
+      return true;
+    } catch (error: any) {
+      console.log("error in cancel order ", error);
+      return error.message;
+    }
+  }
   private async getRestaurantIdFromFood(foodId: string): Promise<number> {
     const food = await prisma.food.findUnique({ where: { id: foodId } });
     return food?.restaurantId ?? 0;
