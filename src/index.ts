206ce06b64cee90a2138c21a0599583087ba1462
Talha Sadiq <talhak911>
2024-09-14 21:18:02 +0500

    fix: omit some fields in signup for security

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ const main = async () => {
   const server = createServer(yoga);
 
   server.listen(4000, () => {
-    console.info("Server is running on http://localhost:4000/graphql");
+    console.info("Server is running");
   });
 };
 main();
