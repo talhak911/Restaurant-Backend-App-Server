8002f5d3ce8a5d61edcd662ea2567bc873898dea
Talha Sadiq <talhak911>
2024-10-11 11:26:12 +0500

    feat: add delete get customer address

--- src/resolvers/customerResolver.ts
+++ src/resolvers/customerResolver.ts
@@ -3,12 +3,17 @@ import {
   Ctx,
   Info,
   Mutation,
+  Query,
   Resolver,
   UseMiddleware,
 } from "type-graphql";
-
+import { v4 as uuidv4 } from "uuid";
 import { isAuth } from "../middleware/middleware";
-import { MyContext } from "../types/types";
+import {
+  CustomerAddress,
+  CustomerAddressType,
+  MyContext,
+} from "../types/types";
 import {
   UpdateOneUserArgs,
   UpdateOneUserResolver,
@@ -18,31 +23,87 @@ import { GraphQLResolveInfo } from "graphql/type";
 @Resolver()
 @UseMiddleware(isAuth)
 export class CustomerResolver {
+  @Query(() => [CustomerAddress])
+  async getCustomerAddress(@Ctx() ctx: MyContext): Promise<CustomerAddress[]> {
+    try {
+      const userId = ctx?.user?.id;
+      const customer = await ctx.prisma.customer.findUniqueOrThrow({
+        where: { id: userId },
+        select: { address: true },
+      });
+
+      return customer.address as CustomerAddressType[];
+    } catch (error: any) {
+      throw new Error("Error while adding address: " + error.message);
+    }
+  }
   @Mutation(() => Boolean || String)
   async addCustomerAddress(
+    @Arg("name") name: string,
     @Arg("address") address: string,
     @Ctx() ctx: MyContext
   ): Promise<boolean | string> {
     try {
       const userId = ctx?.user?.id;
-      await ctx.prisma.customer.findUniqueOrThrow({
-        where: { id: userId },
-      });
+      const newAddress = {
+        id: uuidv4(),
+        name,
+        address,
+      };
 
       await ctx.prisma.customer.update({
         where: { id: userId },
         data: {
           address: {
-            push: address,
+            push: newAddress,
           },
         },
       });
 
       return true;
     } catch (error: any) {
-      throw new Error("Error while adding address " + error.message);
+      if (error.code === "P2025") {
+        throw new Error("Customer not found");
+      }
+      throw new Error("Error while adding address: " + error.message);
     }
   }
+
+  @Mutation(() => Boolean || String)
+  async deleteCustomerAddress(
+    @Arg("addressId") addressId: string,
+    @Ctx() ctx: MyContext
+  ): Promise<boolean | string> {
+    try {
+      const userId = ctx?.user?.id;
+      const customer = await ctx.prisma.customer.findUniqueOrThrow({
+        where: { id: userId },
+      });
+
+      const addresses = customer.address as Array<CustomerAddressType>;
+
+      const addressExists = addresses.some((addr) => addr.id === addressId);
+      if (!addressExists) {
+        throw new Error("Address not found");
+      }
+      const updatedAddresses = addresses.filter(
+        (addr) => addr.id !== addressId
+      );
+      await ctx.prisma.customer.update({
+        where: { id: userId },
+        data: {
+          address: {
+            set: updatedAddresses,
+          },
+        },
+      });
+
+      return true;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
   @Mutation(() => Boolean || String)
   async updateCustomer(
     @Arg("name", { nullable: true }) name: string,
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 import { YogaInitialContext } from "graphql-yoga";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class CustomerAddress {
+  @Field()
+  id!: string;
+
+  @Field()
+  name!: string;
+
+  @Field()
+  address!: string;
+}
 
 export type MyContext = {
   prisma: PrismaClient;
@@ -9,3 +22,5 @@ export type MyContext = {
     role: string;
   };
 } & YogaInitialContext;
+
+export type CustomerAddressType = { id: string; name: string; address: string }
